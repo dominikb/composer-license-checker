4fd2741a80b2611f57fa5722377bc3377ceadc06
Dominik Bauernfeind <bauernfeind.dominik@gmail.com>
2024-01-05 16:10:37 +0100

    Use JSON output for composer licenses command (#37)
    * Use JSON output for composer licenses command
    
    Use a well-known format instead of manually parsing text to extract the required information.

--- tests/DependencyLoaderTest.php
+++ tests/DependencyLoaderTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Dominikb\ComposerLicenseChecker\Tests;
 
+use Dominikb\ComposerLicenseChecker\Contracts\DependencyParser;
 use Dominikb\ComposerLicenseChecker\DependencyLoader;
 use Mockery;
 
@@ -12,28 +13,31 @@ class DependencyLoaderTest extends TestCase
     /** @test */
     public function it_runs_the_command_with_the_given_inputs()
     {
-        $loader = Mockery::mock(DependencyLoader::class)
-                         ->shouldAllowMockingProtectedMethods()
+        $loader = Mockery::mock(DependencyLoader::class, [$this->createNoOpParser()])
                          ->makePartial();
 
         $command = '';
-        $loader->shouldReceive('exec')
+        $loader->shouldAllowMockingProtectedMethods()
+               ->shouldReceive('exec')
                ->once()
                ->withArgs(function ($c) use (&$command) {
                    return (bool) ($command = $c);
                })
-               ->andReturn([
-                   'Name: dominikb/composer-license-checker',
-                   'Version: dev-master',
-                   'Licenses: MIT',
-                   'Dependencies:',
-                   '',
-                   'Name                                  Version              License   ',
-                   'dominikb/composer-license-checker     dev-master f12312    MIT',
-               ]);
+               ->andReturn([]);
 
         $loader->loadDependencies('./composerpath/composer-binary', '/some/directory');
 
-        $this->assertEquals('./composerpath/composer-binary licenses -d /some/directory', $command);
+        $this->assertEquals('./composerpath/composer-binary licenses -f json -d /some/directory', $command);
+    }
+
+    public function createNoOpParser(): DependencyParser
+    {
+        return new class implements DependencyParser
+        {
+            public function parse(string $dependencyOutput): array
+            {
+                return [];
+            }
+        };
     }
 }
---
+++ tests/JSONDependencyParserTest.php
@@ -0,0 +1,141 @@
+<?php
+
+declare(strict_types=1);
+
+use Dominikb\ComposerLicenseChecker\Dependency;
+use Dominikb\ComposerLicenseChecker\JSONDependencyParser;
+use Dominikb\ComposerLicenseChecker\Tests\TestCase;
+
+class JSONDependencyParserTest extends TestCase
+{
+    /** @var \Dominikb\ComposerLicenseChecker\Contracts\DependencyParser */
+    private $parser;
+
+    private $STUB = <<<'JSON'
+        {
+            "name": "dominikb/composer-license-checker",
+            "version": "2.5.0",
+            "license": [
+                "MIT"
+            ],
+            "dependencies": <<<DEPENDENCIES>>>
+        }
+        JSON;
+
+    public function __construct($name = null, array $data = [], $dataName = '')
+    {
+        parent::__construct($name, $data, $dataName);
+
+        $this->parser = new JSONDependencyParser();
+    }
+
+    /** @test */
+    public function it_parses_a_simple_dependency()
+    {
+        $output = $this->dependenciesOutput([
+            'dominikb/composer-license-checker' => [
+                'version' => '2.5.0',
+                'license' => ['MIT'],
+            ],
+        ]);
+
+        $this->assertDependencyMatches(
+            $this->parser->parse($output)[0],
+            'dominikb/composer-license-checker',
+            '2.5.0',
+            'MIT'
+        );
+    }
+
+    /** @test */
+    public function it_parses_versions_without_the_prefix()
+    {
+        $output = $this->dependenciesOutput([
+            'dominikb/composer-license-checker' => [
+                'version' => '2.5.0',
+                'license' => ['MIT'],
+            ],
+        ]);
+
+        $this->assertDependencyMatches(
+            $this->parser->parse($output)[0],
+            'dominikb/composer-license-checker',
+            '2.5.0',
+            'MIT'
+        );
+    }
+
+    /** @test */
+    public function it_handles_dependencies_on_branches()
+    {
+        $output = $this->dependenciesOutput([
+            'dominikb/composer-license-checker' => [
+                'version' => 'dev-test 16af31f',
+                'license' => ['MIT'],
+            ],
+        ]);
+
+        $this->assertDependencyMatches(
+            $this->parser->parse($output)[0],
+            'dominikb/composer-license-checker',
+            'dev-test 16af31f',
+            'MIT'
+        );
+    }
+
+    /** @test */
+    public function it_parses_a_version_without_a_patch_number()
+    {
+        $output = $this->dependenciesOutput([
+            'dominikb/composer-license-checker' => [
+                'version' => '2.5',
+                'license' => ['MIT'],
+            ],
+        ]);
+
+        $this->assertDependencyMatches(
+            $this->parser->parse($output)[0],
+            'dominikb/composer-license-checker',
+            '2.5',
+            'MIT'
+        );
+    }
+
+    /** @test */
+    public function it_parses_multiple_licenses_per_dependency()
+    {
+        $output = $this->dependenciesOutput([
+            'dominikb/composer-license-checker' => [
+                'version' => '2.5',
+                'license' => ['LGPL-2.1-only', 'GPL-3.0-or-later'],
+            ],
+        ]);
+
+        $this->assertDependencyMatches(
+            $this->parser->parse($output)[0],
+            'dominikb/composer-license-checker',
+            '2.5',
+            'LGPL-2.1-only',
+            'GPL-3.0-or-later'
+        );
+    }
+
+    private function dependenciesOutput(array $dependencies): string
+    {
+        return str_replace('<<<DEPENDENCIES>>>', json_encode($dependencies), $this->STUB);
+    }
+
+    /**
+     * @param  Dependency  $dependency
+     * @param  string  $name
+     * @param  string  $version
+     * @param  string[]  $licenses
+     */
+    private function assertDependencyMatches(Dependency $dependency, string $name, string $version, ...$licenses)
+    {
+        $this->assertSame($name, $dependency->getName());
+        $this->assertSame($version, $dependency->getVersion());
+
+        $this->assertEquals($licenses, $dependency->getLicenses());
+    }
+}
